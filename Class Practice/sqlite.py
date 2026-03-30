a8d60cd1ed1e0bfbcd957db1a87af604b1f4f481
anerisedani <anerisedani@gmail.com>
2026-03-30 15:10:02 +0530

    commit

---
+++ Class Practice/sqlite.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+db=sqlite3.connect("task1.db")
+print("database connected!")
+
+#create table
+create_tbl="create table studinfo(id integer primary key autoincrement, name text, city text)"
+db.execute(create_tbl)
+print("table created")
+
+#insert data
